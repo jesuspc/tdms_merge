852ed2a3644207c4f58dad5f23ef584f78952a39
Jesús Prieto <jesus@lunarenergy.com>
2023-07-26 16:51:10 -0700

    Path management

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import sys
 import os
 
 if __name__ == "__main__":
-    input_folder = sys.argv[1]
+    input_folder = os.path.abspath(sys.argv[1])
     output_file = sys.argv[2]
     print("== Welcome to this Amazing TDMS Merge Tool ==")
     print(f"Merging files in folder {input_folder}")
     print(f"Output file is {output_file}")
 
     dir_list = os.listdir(input_folder)
-    dir_list.sort()
+    files = sorted(dir_list, key=lambda f: os.path.getmtime(os.path.join(input_folder, f)))
 
     dfs = []
-    for f in dir_list:
-        tdms = TdmsFile.read(f"{input_folder}/{f}")
+    for f in files:
+        tdms = TdmsFile.read(os.path.join(input_folder, f))
         dfs.append(tdms.as_dataframe())
 
     df = pd.concat(dfs)
